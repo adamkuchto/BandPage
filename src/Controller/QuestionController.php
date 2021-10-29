446a79c5f5cc5c854f7fff4a2143a6d735a4d303
Adam Kuchto <adamkuchto@gmail.com>
2021-10-30 00:40:40 +0200

    firs step

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+class QuestionController
+{
+    /**
+     * @Route ("/")
+     */
+    public function homepage()
+    {
+        return new Response('Zespół Muzyki Dawnej "Góra Trolla"');
+    }
+
+    /**
+     * @Route ("/O nas/co-to-za-zespół")
+     */
+    public function aboutUs()
+    {
+        return new Response('strona info o zespole');
+    }
+
+}
